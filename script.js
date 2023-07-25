dcc4d9c982fccb5793bca08f644de3d1915a02e1
user3456tyhjvv <137161597+user3456tyhjvv@users.noreply.github.com>
2023-07-25 11:48:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element by its id
+const button = document.getElementById('clickMeButton');
+
+// Add a click event listener to the button
+button.addEventListener('click', () => {
+    // Display an alert when the button is clicked
+    alert('You clicked the button!');
+});
